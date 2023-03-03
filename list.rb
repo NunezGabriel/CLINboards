4d243761c80255b406d5b02d5ffb6b670da727a0
Alex Mendoza <alex.mendoza.et@gmail.com>
2023-03-03 14:49:50 -0500

    add: list Class

--- list.rb
+++ list.rb
@@ -3,62 +3,76 @@ require "json"
 
 class List
   attr_reader :list
+
+  @@id = nil
+
   def initialize()
-    @list = []  
+    @list = []
   end
 
+
   def create_list(value)
-    hash = {}
-    hash[value] = {}
+    hash = {id: next_id, name:value, cards: []}
     @list << hash
   end
 
-#########################################
-  #falta revisar este uptate
-  def update_list(value)
-    list = @list.map do |name|
-              if name.keys.include?(:value)
-              end
-            end
-    list[0].transform_keys! { |key| key == "value" ? "newvalue" : key }
+  def update_list(name, new_name)
+    @list.find do |hash|
+      if hash[:name] == name
+        hash[:name] = new_name
+      end
+    end
   end
 
   def delete_list(value)
-    @list.delete_if{|key, _value| key == value}
+    @list.delete_if{|hash| hash[:name] == value}
   end
-#########################################
 
-  def create_card(value)
+  def create_card(name_list = "todo", value)
     newcard = Cards.new(**value)
-    @list << newcard
-    @list
+    @list.find do |hash|
+      if hash[:name] == name_list
+        hash[:cards] << newcard
+      end
+    end
   end
 
-  def update(id,value)
-    card = @list.find { |card| card.id == id }
-    card.update_card(**value)
+  def update_cards(id,name_list,value)
+    cards = @list.find { |hash| hash[:cards].find {|card| card.id == id }}
+
+    cards[:cards].each do |card|
+      if card.id == id
+       card.update_card(**value)
+      end
+    end
+
+    new_card = @list.find { |hash| hash[:cards].find {|card| card.id == id }}
+
+    @list.each do |hash|
+      if hash[:name] != name_list
+        new_hash = @list.find { |hash| hash[:name] == name_list }
+        new_hash[:cards].concat(new_card[:cards])
+        break
+      end
+    end
+
+    cards[:cards].delete_if { |card| card.id == id }
   end
-  
-  def delete(id)
-    @list.delete_if{|card| card.id == id}
+
+  def delete_card(id)
+    cards = @list.find { |hash| hash[:cards].find {|card| card.id == id }}
+    cards[:cards].delete_if { |card| card.id == id }
   end
 
   private
 
-  def load
-    playlists_data = JSON.parse(File.read("card.json"), symbolize_names: true )
+  # def load
+  #   playlists_data = JSON.parse(File.read("card.json"), symbolize_names: true )
+  # end
+
+  def next_id
+    @@id = @list.size + 1
   end
+
 end
 
-lista = List.new()
- data = 
-  {
-    title: "pepe",
-    members: ["Diego", "Deyvi", "Wences"],
-    labels: ["investigate"],
-    due_date: "2023-02-19",
-  }
-
-
-lista.create_list(:casa)
-# p lista.list
